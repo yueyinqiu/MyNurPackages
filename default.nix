041f3c7b41097c7db62e7006bbe998b6b2f88da6
yueyinqiu <yueyinqiu@outlook.com>
2026-08-04 14:50:33 +0800

    add to default

--- default.nix
+++ default.nix
@@ -20,6 +20,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  mihomo-manager-mihomo-mixin = pkgs.callPackage ./pkgs/mihomo-manager-mihomo-mixin { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
